81e02fecd38764f55ca5d4c7df3fe519fedc6a8a
ebegoflo <ebenezergf@gmail.com>
2018-03-06 09:29:33 -0600

    pintando pokemones

--- js/app.js
+++ js/app.js
@@ -5,25 +5,43 @@ var theUrl = "https://pokeapi.co/api/v2/pokemon/?limit=" + limit + "&offset=" + 
 $.ajax({
   dataType : "json",
   url:  theUrl
-}).done(getPokemon).fail(function( xhr, status) {
-  console.log( "Error" );
+}).done(getPokemon).fail(function (status) {
+  console.log("error");
 })
-// console.log(forms);
-// $( this ).addClass( "done" );
 
-function setPaginator(){
-  $('.prev').click(function (){
 
+function modalInfo(){
+  $('#modal').click(function (namePokemon){
+    $.ajax({
+      dataType : "json",
+      url:  `https://pokeapi.co/api/v2/pokemon/pokemon${namePokemon}`
+    }).done(function(data){
+      console.log(data)
+    })
   });
 
 }
+
+
+function pokemonDetail (namePokemon) {
+    $.ajax({
+      dataType : "json",
+      url:  `https://pokeapi.co/api/v2/pokemon-form/${namePokemon}`
+    }).done(function(data){
+      console.log(data.name);
+      console.log(data.sprites.front_default);
+      $('.img-pokemon-' + data.name).attr('src', data.sprites.front_default);
+    })
+}
+
+
+
 
 function getPokemon (data) {
   var obj = data
   var arr = data.results;
   // console.log(arr);
   // console.log(obj);
-
   $.each(arr, function(i,value){
       var infoPokemon = value;
       // console.log(infoPokemon);
@@ -32,9 +50,10 @@ function getPokemon (data) {
       // console.log(namePokemon);
       // console.log(urlPokemon);
 
-      paintPokemon(namePokemon, urlPokemon);
+      // pokemonDetail (namePokemon);
+      paintPokemon(namePokemon);
+      pokemonDetail (namePokemon)
   })
-
 
 //posible funcion para paginator
   // $.each( obj, function( key, value ) {
@@ -48,22 +67,35 @@ function getPokemon (data) {
   // });
 }
 
-function paintPokemon(namePokemon, urlPokemon) {
+function paintPokemon(namePokemon,urlPokemon) {
 
-  var $row = $("<div />").addClass("row");
-  var $namePokemon = $("<h5 />").addClass("center-align flow-text col s12 m4 l4");
-  var $url = $("<a />").addClass("center-align flow-text col s12 m4 l4");
-  var $image = $("<img />").addClass("col s12 m4 l4 center-align responsive-img");
+  let templete = `<div class="panel panel-default col-md-3">
+  <div class="panel-heading text-center">
+  <img class="responsive-img img-rounded img-pokemon-${namePokemon}" src="${urlPokemon}" alt="${namePokemon}">
+  </div>
+  <div class="panel-body">
+  <h3 class="panel-title">${namePokemon}</h3>
+  <a id="modal" href="${urlPokemon}">Conoce más</a>
+  </div>
+  </div>`
 
-  $url.prop("href",urlPokemon);
-  $image.prop("src","https://dummyimage.com/200x200")
-  $namePokemon.text(namePokemon);
+  const pokemonContainer = document.getElementById('container_Pokemon')
+  pokemonContainer.innerHTML += templete;
 
-  $row.append($namePokemon);
-  $row.append($url);
-  $row.append($image);
-
-  $("#container_Pokemon").append($row);
+  // var $row = $("<div />").addClass("row");
+  // var $namePokemon = $("<h5 />").addClass("center-align flow-text col s12 m3 l3");
+  // var $url = $("<a />").addClass("center-align flow-text col s12 m3 l3");
+  // var $image = $("<img />").addClass("col s12 m3 l3 center-align responsive-img");
+  //
+  // $url.prop("href",urlPokemon).text("Conoce más");
+  // $image.prop("src","https://dummyimage.com/200x200")
+  // $namePokemon.text(namePokemon);
+  //
+  // $row.append($namePokemon);
+  // $row.append($url);
+  // $row.append($image);
+  //
+  // $("#container_Pokemon").append($row);
 
 }
 
